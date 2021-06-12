dfb6b3d3577dec6a680dad1d10eb8913e2ce4812
MoMoPouetDev <morgan.venandy@gmail.com>
2021-06-12 13:44:02 +0100

    add mqtt communication

--- serveurWeb/api.py
+++ serveurWeb/api.py
@@ -2,25 +2,45 @@
 
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS, cross_origin
+from flask_mqtt import Mqtt
 from threading import Thread
 from bluepy import btle
+from logging.handlers import RotatingFileHandler
 
+import paho.mqtt.client as mqtt
 import binascii
 import json
 import time
 import os
+import datetime
+import logging
 
 import status
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+file_handler = RotatingFileHandler('activity.log', 'a', 1000000, 1)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
 app = Flask(__name__)
 CORS(app)
+
 app.config['SECRET_KEY'] = 'cleSecrete'
 app._static_folder = os.path.abspath("templates/static/")
+
+broker_address="192.168.1.108"
+mqtt.Client.connected_flag=False
 
 thread = None
 data_ble = [b'00',b'00',b'00',b'00',b'00',b'00',b'00',b'00',b'00',b'00',b'00',b'00',b'00',b'00',b'00',b'00',b'00',b'00',b'00']
 mower = None
-flagDisconnect = False
+client = None
+flagBleDisconnect = False
+flagMqttDisconnect = False
+flagMqttReceived = False
 
 class MyDelegate(btle.DefaultDelegate):
     def __init__(self,params):
@@ -31,77 +51,125 @@ class MyDelegate(btle.DefaultDelegate):
             data_ble[i] = binascii.b2a_hex(data[i:(i+1)])
 
 def back_thread():
-    global flagDisconnect
-    global thread
-
+    global flagBleDisconnect
+    global flagMqttReceived
+    test = 0
     connect_mower()
     mower.setDelegate(MyDelegate(0))
     while True:
+        test = test + 1
         try:
             if mower.waitForNotifications(0.5):
-                flagDisconnect = False
+                logger.debug("BLE Connected")
+                client.publish("PUBmower",test)
+                flagBleDisconnect = False
+                flagMqttReceived = False
                 continue
         except btle.BTLEDisconnectError:
-            print("Connection Error")
-            flagDisconnect = True
-            thread = None
-            pass
+            logger.debug("BLE Disconnected")
+            flagBleDisconnect = True
+            return back_thread()
 
 def connect_mower():
     global mower
+    global thread
     try:
         mower = btle.Peripheral('50:33:8B:F8:5A:90')
     except Exception as e:
-        print("Connection Error",e)
-        return connect_mower()
+        logger.error("Connection Error",e)
+        thread = None 
+        return launch_thread()
+
+def decode_data_ble():
+    return status.decodeReceivedData(data_ble)
+
+def send_command_ble(command):
+    mowerService = mower.getServiceByUUID("0000ffe0-0000-1000-8000-00805f9b34fb")
+    mowerCharac = mowerService.getCharacteristics()[0]
+
+    mowerCharac.write(bytes(command, 'utf-8'))
+
+def launch_thread():
+    global thread
+    thread = Thread(target=back_thread)
+    thread.start()
+
+def stop_thread():
+    thread.join()
+
+def on_connect_mqtt(client, userdata, flags, rc):
+    if rc==0:
+        client.connected_flag=True
+        logger.debug("connected OK")
+    else:
+        logger.error("Bad connection Returned code=",rc)
+
+def on_message_mqtt(client, userdata, message):
+    global flagMqttReceived
+    command = str(message.payload.decode("utf-8"))
+    print("message received " ,command)
+    flagMqttReceived = True 
+    if command=="start" and not flagMqttReceived:
+        send_command_ble('1')
+    if command=="stop" and not flagMqttReceived:
+        send_command_ble('2')
+
+def start_mqtt():
+    global client
+    
+    client = mqtt.Client()
+    client.on_connect = on_connect_mqtt
+    client.on_message = on_message_mqtt
+    client.connect(broker_address, 1883)
+    client.loop_start()
+    
+    while not client.connected_flag:
+        logger.debug("Wait")
+        time.sleep(1)
+    
+    client.subscribe("SUBmower")    
+
+def stop_mqtt():
+    client.loop_stop()
+    client.disconnect()
 
 @app.route('/') 
 def sessions():
-        global thread
-        if thread is None:
-                thread = Thread(target=back_thread)
-                thread.start()
-        return render_template("index.html")
+    if thread is None:
+        launch_thread()
+    return render_template("index.html")
 
 @app.route('/ble', methods=['GET', 'POST'])
 def ble():
     if request.method == 'GET':
-        if flagDisconnect == True:
-            print("TEST")
+        if flagBleDisconnect == True:
+            logger.debug("BLE Disconnected")
             return jsonify(connection="Error")
 
-        if flagDisconnect == False:
-            data_json = status.decodeReceivedData(data_ble)
-            print("JSON ",data_json)
+        if flagBleDisconnect == False:
+            data_json = decode_data_ble()
+            logger.debug("JSON ",data_json)
             return jsonify(data_json)
 
     if request.method == 'POST':
-        print(request.get_json())  # parse as JSON
+        logger.debug(request.get_json())  # parse as JSON
         return jsonify(status=200)
 
 @app.route('/start', methods=['POST'])
 def commandStart():
     if request.method == 'POST':
-        print(request.get_json())  # parse as JSON
-
-        mowerService = mower.getServiceByUUID("0000ffe0-0000-1000-8000-00805f9b34fb")
-        mowerCharac = mowerService.getCharacteristics()[0]
-
-        mowerCharac.write(bytes('1', 'utf-8'))
-
+        logger.debug(request.get_json())  # parse as JSON
+        send_command_ble('1')
         return jsonify(status=200)
 
 @app.route('/stop', methods=['POST'])
 def commandStop():
     if request.method == 'POST':
-        print(request.get_json())  # parse as JSON
-
-        mowerService = mower.getServiceByUUID("0000ffe0-0000-1000-8000-00805f9b34fb")
-        mowerCharac = mowerService.getCharacteristics()[0]
-
-        mowerCharac.write(bytes('2', 'utf-8'))
-
+        logger.debug(request.get_json())  # parse as JSON
+        send_command_ble('2')
         return jsonify(status=200)
+
+start_mqtt()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
